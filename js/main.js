034b3bd03ecbe6fa9d607a3701be233136761a71
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-18 16:16:28 +0200

    feat: add a for cicle for print in console the object values

--- js/main.js
+++ js/main.js
@@ -1,38 +1,51 @@
+// creo l'array con tutti gli ogetti dentro che sono le informazioni dei membri del team
 const teamMembers = [
   {
-    nome: "Wayne",
-    cognome: "Barnett",
+    nome: "Wayne Barnett",
     ruolo: "Founder & CEO",
     img: "wayne-barnett-founder-ceo.jpg",
   },
   {
-    nome: "Angela",
-    cognome: "Caroll",
+    nome: "Angela Caroll",
     ruolo: "Chief Editor",
     img: "angela-caroll-chief-editor.jpg",
   },
   {
-    nome: "Walter",
-    cognome: "Gordon",
+    nome: "Walter Gordon",
     ruolo: "Office Manager",
     img: "walter-gordon-office-manager.jpg",
   },
   {
-    nome: "Angela",
-    cognome: "Lopez",
+    nome: "Angela Lopez",
     ruolo: "Social Media Manager",
     img: "angela-lopez-social-media-manager.jpg",
   },
   {
-    nome: "Scott",
-    cognome: "Estrada",
+    nome: "Scott Estrada",
     ruolo: "Developer",
     img: "scott-estrada-developer.jpg",
   },
   {
-    nome: "Barbara",
-    cognome: "Ramos",
+    nome: "Barbara Ramos",
     ruolo: "Graphic Designer",
     img: "barbara-ramos-graphic-designer.jpg",
   },
 ];
+
+// creo un ciclo for per estrapolare gli ogetti dell'array
+for (let i = 0; i < teamMembers.length; i++) {
+  let member = teamMembers[i];
+  //   console.log(member);
+
+  // creo le variabili dove inserisco i vari valori
+  let memberName = member.nome;
+  let memberRole = member.ruolo;
+  let memberImg = member.img;
+
+  // stampo in console i valori degli ogetti nome e cognome ruolo immagine
+  console.log(
+    "Nome e cognome: " + memberName,
+    "Ruolo: " + memberRole,
+    "Immagine: " + memberImg
+  );
+}
